e8444fd43514496d664876ec918e758691bc085f
BurningTreeC <huber.simon@protonmail.com>
2020-12-26 20:52:22 +0100

    updates

--- BTC/Muuri/files/modules/storyviews/muuri.js
+++ BTC/Muuri/files/modules/storyviews/muuri.js
@@ -56,7 +56,8 @@ var MuuriStoryView = function(listWidget) {
 				var style = item.getElement().style;
 				style.width = '';
 				style.height = '';
-				self.muuri.refreshItems([item]);
+				self.refreshMuuriGrid();
+				//self.muuri.refreshItems([item]);
 			})
 			.on("add",function(items) {
 				self.updateZIndexList();
